c5cec415ae836c618f277246bf66f66afd1bcfa1
Alex Chipre <alechipre@gmail.com>
2024-01-12 14:22:00 -0500

    Add Icon and API

--- src/components/SectionWeatherToday.jsx
+++ src/components/SectionWeatherToday.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+
 export default function SectionWeatherToday () {
+  const URL = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid='
+  const KEY = 'a694f184579ce9404b9ff87dbc046b5d'
+  useEffect(() => {
+    fetch(URL + KEY)
+      .then(resp => resp.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <article>SectionWeaterToday</article>
   )
