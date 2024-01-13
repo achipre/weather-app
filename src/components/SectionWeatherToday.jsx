04608cf5568c7ac1fb6f42aa706300f02ebdc417
Alex Chipre <alechipre@gmail.com>
2024-01-13 12:00:48 -0500

    fix api

--- src/components/SectionWeatherToday.jsx
+++ src/components/SectionWeatherToday.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
+import { IconsSearch } from './Icons'
+import './sectionWeatherToday.css'
 
 export default function SectionWeatherToday () {
-  const URL = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid='
-  const KEY = 'a694f184579ce9404b9ff87dbc046b5d'
-  useEffect(() => {
-    fetch(URL + KEY)
-      .then(resp => resp.json())
-      .then(data => console.log(data))
-  }, [])
+  // const URL = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid='
+  // useEffect(() => {
+  //   fetch(URL + KEY)
+  //     .then(resp => resp.json())
+  //     .then(data => console.log(data))
+  // }, [])
 
   return (
-    <article>SectionWeaterToday</article>
+    <article className='weatherToday'>
+      <IconsSearch />
+    </article>
   )
 }
