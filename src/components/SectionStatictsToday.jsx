2a52293d08a56a271262578bf3213c153b16d5a9
Alex Chipre <alechipre@gmail.com>
2024-01-20 17:14:28 -0500

    Add Icons Highlight

--- src/components/SectionStatictsToday.jsx
+++ src/components/SectionStatictsToday.jsx
@@ -1,8 +1,49 @@
+import { IconsTermometer, IconsVisibility } from './Icons'
 import './sectionStaticsToday.css'
 export default function SectionStatictsToday ({ isSection }) {
   return (
-    <article className={isSection === 'PinMap' ? 'articleFull' : 'weatherTwo'}>
-      <h2>2Today&apos;s HightLight</h2>
+    <article className={`cardTodayHighlight ${isSection === 'PinMap' ? 'articleFull' : 'weatherTwo'}`}>
+      <h2>TODAY&apos;S HIGHLIGHT</h2>
+      <div className="weatherTodayHighlight">
+        <article className='cardToday cardWind'>
+          <p>Wind Status</p>
+          <p>7.5</p><span>km/h</span>
+        </article>
+        <article className='cardToday cardUV'>
+          <p>UV Index</p>
+          <p>5.50</p><span>uv</span>
+        </article>
+        <article className='cardToday cardPressure'>
+          <p>Pressure</p>
+          <p>1020</p><span>hPa</span>
+        </article>
+        <article className='cardToday cardHumanity'>
+          <p>Humidity</p>
+          <p>80</p><span>%</span>
+        </article>
+        <article className='cardToday cardVisivility'>
+          <p>Visibility</p>
+          <div className="statics">
+            <div className="visibility">
+              <p>1000<span>m</span></p>
+            </div>
+            <p className='descriptions'>
+              <IconsVisibility />
+              Visibility in meter</p>
+          </div>
+        </article>
+        <article className='cardToday cardFeelsLike'>
+          <p>Feels Like</p>
+          <div className="statics">
+            <div className="temp">
+              <IconsTermometer />
+              <p>10<sup>o</sup></p>
+            </div>
+            <p className='descriptions'>Temperature human perception</p>
+
+          </div>
+        </article>
+      </div>
     </article>
   )
 }
