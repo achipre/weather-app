807e4d8cd4c2d6aae2d09ddb8b893e3ad05ef3f0
Alex Chipre <alechipre@gmail.com>
2024-01-25 17:33:23 -0500

    Add Section wind

--- src/components/SectionStatictsToday.jsx
+++ src/components/SectionStatictsToday.jsx
@@ -14,15 +14,17 @@ export default function SectionStatictsToday ({ isSection }) {
             <div className="wind">
               <div className="windAnimation">
                 <div className="windLine"></div>
+                <div className="windLine2"></div>
                 <div className="windLine"></div>
+                <div className="windLine2"></div>
                 <div className="windLine"></div>
+                <div className="windLine2"></div>
                 <div className="windLine"></div>
+                <div className="windLine2"></div>
                 <div className="windLine"></div>
+                <div className="windLine2"></div>
                 <div className="windLine"></div>
-                <div className="windLine"></div>
-                <div className="windLine"></div>
-                <div className="windLine"></div>
-                <div className="windLine"></div>
+                <div className="windLine2"></div>
                 <div className="windLine"></div>
               </div>
               <p>7.5<span>km/h</span></p>
