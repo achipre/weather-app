1a1bdea38ffcfbae0b01a6f5a98d4fc416f0824e
Alex Chipre <alechipre@gmail.com>
2024-01-29 18:07:05 -0500

    Add info wind

--- src/components/SectionWeatherMap.jsx
+++ src/components/SectionWeatherMap.jsx
@@ -3,6 +3,7 @@ export default function SectionWeatherMap ({ isSection }) {
   return (
     <article className={isSection === 'Calendar' ? 'articleFull' : 'weatherFour'}>
       <h2>WEATHER&apos;S IN THE MAP</h2>
+
     </article>
   )
 }
